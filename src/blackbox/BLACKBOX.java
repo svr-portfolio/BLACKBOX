e26d13d334b6cf13fe8ef8cc817c020830ea5d19
Tapaswika <varshinireddy@Tapaswikas-MacBook-Pro.local>
2026-09-18 16:28:11 +0530

    Initial commit

---
+++ src/blackbox/BLACKBOX.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package blackbox;
+
+public class BLACKBOX {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("          BLACKBOX");
+        System.out.println(" Accident Reconstruction Engine");
+        System.out.println("=================================");
+        System.out.println("BLACKBOX started successfully!");
+
+    }
+}
